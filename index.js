43241fee6350f4dfd4988d6cd371e507826d98d6
faezeh panahi <faezeh.panahi@gmail.com>
2022-03-07 14:36:06 +0330

    used Redux cdn

--- index.js
+++ index.js
@@ -83,44 +83,44 @@ function goals(state = [], action) {
   }
 }
 
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  };
-}
+// function app(state = {}, action) {
+//   return {
+//     todos: todos(state.todos, action),
+//     goals: goals(state.goals, action),
+//   };
+// }
 
-function createStore() {
-  // the store shuold have 4 parts:
-  // 1. the state
-  // 2. get the state
-  // 3. listen to change on the state
-  // 4. update the state
+// function createStore() {
+//   // the store shuold have 4 parts:
+//   // 1. the state
+//   // 2. get the state
+//   // 3. listen to change on the state
+//   // 4. update the state
 
-  let state;
-  let listeners = [];
+//   let state;
+//   let listeners = [];
 
-  const getState = () => state;
+//   const getState = () => state;
 
-  const subscribe = (listener) => {
-    listeners.push(listener);
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
 
-    return () => {
-      listeners = listeners.filter((item) => item !== listener);
-    };
-  };
+//     return () => {
+//       listeners = listeners.filter((item) => item !== listener);
+//     };
+//   };
 
-  const dispatch = (action) => {
-    state = app(state, action);
-    listeners.forEach((listener) => listener());
-  };
+//   const dispatch = (action) => {
+//     state = app(state, action);
+//     listeners.forEach((listener) => listener());
+//   };
 
-  return {
-    getState,
-    subscribe,
-    dispatch,
-  };
-}
+//   return {
+//     getState,
+//     subscribe,
+//     dispatch,
+//   };
+// }
 
 // Dom Code
 
@@ -159,7 +159,12 @@ function addGoal() {
 document.getElementById("todoBtn").addEventListener("click", addTodo);
 document.getElementById("goalBtn").addEventListener("click", addGoal);
 //
-const store = createStore(app);
+const store = Redux.createStore(
+  Redux.combineReducers({
+    todos,
+    goals,
+  })
+);
 
 store.subscribe(() => {
   const { goals, todos } = store.getState();
@@ -171,10 +176,30 @@ store.subscribe(() => {
   goals.forEach(addGolDOM);
 });
 
+function createRemoveButton(onClick) {
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("trash");
+  removeBtn.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>';
+  removeBtn.addEventListener("click", onClick);
+  return removeBtn;
+}
+
 function addTodoDOM(todo) {
   const node = document.createElement("li");
   const text = document.createTextNode(todo.name);
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id));
+  });
+
   node.appendChild(text);
+  node.appendChild(removeBtn);
+
+  node.style.textDecoration = todo.complete ? "line-through" : "none";
+  node.addEventListener("click", () => {
+    store.dispatch(toggleTodoAction(todo.id));
+  });
 
   document.getElementById("todos").appendChild(node);
 }
@@ -182,7 +207,13 @@ function addTodoDOM(todo) {
 function addGolDOM(goal) {
   const node = document.createElement("li");
   const text = document.createTextNode(goal.name);
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeGoalAction(goal.id));
+  });
+
   node.appendChild(text);
+  node.appendChild(removeBtn);
 
   document.getElementById("goals").appendChild(node);
 }
